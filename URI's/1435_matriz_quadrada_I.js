505176cf821abea31f17da9ee734530872cfc4dc
LeoAlves25 <99600394+LeoAlves25@users.noreply.github.com>
2022-05-26 19:43:59 -0300

    Add files via upload

--- URI's/1435_matriz_quadrada_I.js
+++ URI's/1435_matriz_quadrada_I.js
@@ -3,35 +3,39 @@ var lines = input.split('\n');
 var prompt = function(texto){ return lines.shift()}
 
 var M=[];
-while(true){
-    var N=parseInt(prompt());
-    if(N===0){
-        break;
-    }
-        else{
-        for(i=1;i<=N;i++){
-            M[i]=[];
-            var text="";
-            
-            for(j=1;j<=N;j++){
-                M[i][j]=i;
-                if(j<M[i][j]){
-                    M[i][j]=j;
-                }
-                if((N-i+1)<M[i][j]){
-                    M[i][j]=N-i+1;
-                }
-                if((N-j+1)<M[i][j]){
-                    M[i][j]=N-j+1;
-                }
-                if(j==N){
-                    text+=M[i][j];
-                }else{
-                    text+=M[i][j]+" ";
-                }
+var N=parseInt(prompt());
+while(N!==0){
+    for(var i=1;i<=N;i++){
+        M[i]=[];
+        var text="";
+                
+        for(var j=1;j<=N;j++){
+            M[i][j]=i;
+
+            if(j<M[i][j]){
+                M[i][j]=j;
             }
+            if((N-i+1)<M[i][j]){
+                M[i][j]=N-i+1;
+            }
+            if((N-j+1)<M[i][j]){
+                M[i][j]=N-j+1;
+            }
+
+            if(M[i][j]>9 && j!=1){
+                text+="  "+M[i][j];
+            }else if(M[i][j]<=9 && j!=1){
+                text+="   "+M[i][j];
+            }else if(j==1){
+                text+="  "+M[i][j];
+            }
+        }
+        if(i==N){
+            console.log(text+'\n');
+        }else{
             console.log(text);
         }
-        console.log("")
     }
+
+    N=parseInt(prompt());
 }
